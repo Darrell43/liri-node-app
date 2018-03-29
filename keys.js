da1f63ddf142bf3a0f2916a4e52a52d5ec19e871
Darrell Abney <dabney@ceo-psb-dhcp17.rutgers.edu>
2018-03-29 15:39:17 -0400

    Updating Packages

--- keys.js
+++ keys.js
@@ -1,16 +1,23 @@
 console.log('this is loaded');
 
-exports.twitter = {
-  consumer_key: process.env.TWITTER_CONSUMER_KEY,
-  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
-};
+var client = new Twitter( {
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
 
-exports.spotify = {
-  id: process.env.SPOTIFY_ID,
-  secret: process.env.SPOTIFY_SECRET
-};
+var spotify = new Spotify( {
+    id: process.env.SPOTIFY_ID,
+    secret: process.env.SPOTIFY_SECRET,
+    type: 'artist OR album OR track', 
+    query: 'My search query',
+    limit: 20, callback
+});
 
-module.exports = exports.twitter;
-module.exports = exports.spotify;
+
+
+module.exports = {
+    client: new Twitter,
+    spotify: new Spotify,
+}
--- liri.js
+++ liri.js
@@ -2,8 +2,23 @@ require("dotenv").config();
 
 console.log(process.env.TWITTER_CONSUMER_KEY);
 
-var spotify = new Spotify(keys.spotify);
-var client = new Twitter(keys.twitter);
+var Twitter = require('./keys');
+var Spotify = require('./keys');
+
+
+if (process.argv[3]) {
+    var client = process.argv[3];
+}
+
+
+client.get('favorites/list', function (error, tweets, response) {
+    if (error) throw error;
+    console.log(tweets);  // The favorites. 
+    console.log(response);  // Raw response object. 
+});
+
+
+
 
 
 
