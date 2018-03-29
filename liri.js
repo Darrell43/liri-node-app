130314160982f974eaf867a5fb2f88789044d578
Darrell Abney <dabney@ceo-psb-dhcp17.rutgers.edu>
2018-03-29 14:50:18 -0400

    All packages installed

--- liri.js
+++ liri.js
@@ -2,10 +2,9 @@ require("dotenv").config();
 
 console.log(process.env.TWITTER_CONSUMER_KEY);
 
-//var spotify = new Spotify(keys.spotify);
-//var client = new Twitter(keys.twitter);
-//var hmm = process.env;
+var spotify = new Spotify(keys.spotify);
+var client = new Twitter(keys.twitter);
 
 
-//console.log(process.env.TWITTER_CONSUMER_KEY);
+
 
