72bcebf085744697df11255cf80bd7eecc5f1085
Darrell Abney <dabney@ceo-psb-dhcp17.rutgers.edu>
2018-03-29 14:06:42 -0400

    Progress

---
+++ liri.js
@@ -0,0 +1,11 @@
+require("dotenv").config();
+
+console.log(process.env.TWITTER_CONSUMER_KEY);
+
+//var spotify = new Spotify(keys.spotify);
+//var client = new Twitter(keys.twitter);
+//var hmm = process.env;
+
+
+//console.log(process.env.TWITTER_CONSUMER_KEY);
+
