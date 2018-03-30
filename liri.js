9294b7c845090c15d3a8e908d2d30b144c127a35
Darrell Abney <dabney@ceo-psb-dhcp17.rutgers.edu>
2018-03-30 15:18:38 -0400

    updating

--- liri.js
+++ liri.js
@@ -6,17 +6,15 @@ var Twitter = require('./keys');
 var Spotify = require('./keys');
 
 
-if (process.argv[3]) {
-    var client = process.argv[3];
-}
 
-
-client.get('favorites/list', function (error, tweets, response) {
-    if (error) throw error;
-    console.log(tweets);  // The favorites. 
-    console.log(response);  // Raw response object. 
-});
-
+spotify
+  .request('https://api.spotify.com')
+  .then(function(data) {
+    console.log(data); 
+  })
+  .catch(function(err) {
+    console.error('Error occurred: ' + err); 
+  });
 
 
 
